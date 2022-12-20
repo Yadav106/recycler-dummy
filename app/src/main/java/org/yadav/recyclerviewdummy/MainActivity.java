86805052827053d22eb0fc6f3ac51244459a266b
Yadav106 <sahil.jp.yadav@gmail.com>
2022-12-20 18:49:39 +0530

    added data

--- app/src/main/java/org/yadav/recyclerviewdummy/MainActivity.java
+++ app/src/main/java/org/yadav/recyclerviewdummy/MainActivity.java
@@ -29,7 +29,20 @@ public class MainActivity extends AppCompatActivity {
 
     private List<Contact> initData() {
         List<Contact> contacts = new ArrayList<>();
-        contacts.add(new Contact("9324029051", "Sahil Yadav"));
+        contacts.add(new Contact("1275838494", "Hari Puttar"));
+        contacts.add(new Contact("9583859394", "Dumbles Door"));
+        contacts.add(new Contact("4839858494", "Free Elf Dhobi"));
+        contacts.add(new Contact("9349583843", "Her My Knee"));
+        contacts.add(new Contact("8459848582", "Ronald Duck"));
+        contacts.add(new Contact("8395939493", "Zoro"));
+        contacts.add(new Contact("4829385834", "Long Bottom"));
+        contacts.add(new Contact("7349395835", "Luna Fish"));
+        contacts.add(new Contact("4839593858", "No Nose November"));
+        contacts.add(new Contact("4892835829", "Siriusly Black"));
+        contacts.add(new Contact("9693858384", "Dolores Cambridge"));
+        contacts.add(new Contact("2949593959", "Pottahhhh"));
+        contacts.add(new Contact("9385734783", "Victor Krumbles"));
+
         return contacts;
     }
 
